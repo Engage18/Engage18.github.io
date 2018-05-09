a736aa8cbfbda48d8dbabb469a505628054e035e
BradyC125 <bradycrimmins@gmail.com>
2018-05-09 09:11:20 -0400

    Refactor slideshow entirely
    Transform everything to class based system so there's no need to add
    specific instances upon adding a slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,38 +1,58 @@
-function unhideSlide(n, eventName) {
-	console.log("Index passed:")
-	console.log(n)
-	var i;
-	var slides = document.getElementsByClassName(eventName);
-	if (n >= slides.length) {n=0}
-	if (n < 0) {n=slides.length-1}
-	console.log("Index shown:")
-	console.log(n)
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none"; 
+"use strict";
+
+const buttonMap = new WeakMap;
+
+function changeLeft() {
+  shiftSlide(buttonMap.get(this), -1);
+}
+
+function changeRight() {
+  shiftSlide(buttonMap.get(this), 1);
+}
+
+function SlideshowInstance(imageDiv, leftButton, rightButton) {
+  this.leftButton = leftButton;
+  buttonMap.set(leftButton, this);
+  this.rightButton = rightButton;
+  buttonMap.set(rightButton, this);
+  this.imageArray = imageDiv.querySelectorAll("img");
+  this.index = 0;
+  leftButton.addEventListener("click", changeLeft);
+  rightButton.addEventListener("click", changeRight);
+}
+
+function shiftSlide(instance, number) {
+	console.log("Shift slides run");
+	var originalIndex = instance.index;
+	instance.index += number;
+	console.log(instance);
+	console.log(instance.imageArray);
+	if (instance.index == instance.imageArray.length) {instance.index = 0}
+	if (instance.index < 0) {instance.index = instance.imageArray.length - 1}
+	instance.imageArray[originalIndex].style.display = "none";
+	instance.imageArray[instance.index].style.display = "block";
+}
+
+function initSlides(slideshows) {
+	var instances = [];
+	for(var i=0; i<slideshows.length; i++) {
+		console.log(slideshows[i]);
+		var imageDiv = slideshows[i].querySelector(".imageContainer");
+		var leftButton = slideshows[i].querySelector(".prevButton");
+		var rightButton = slideshows[i].querySelector(".nextButton");
+		instances.push(new SlideshowInstance(imageDiv, leftButton, rightButton))
+		console.log(instances[i].imageArray);
 	}
-	slides[n].style.display = "block"; 
-	return n;
+	return instances;
 }
 
-function changeWalkoutSlide(n) {
-	walkoutSlideIndex+=n;
-	walkoutSlideIndex = unhideSlide(walkoutSlideIndex, "walkoutSlides");
-}
-
-function changePanelSlide(n) {
-	panelSlideIndex+=n;
-	panelSlideIndex = unhideSlide(panelSlideIndex, "panelSlides");
-}
-
-function changeDebateSlide(n) {
-	debateSlideIndex+=n;
-	debateSlideIndex = unhideSlide(debateSlideIndex, "debateSlides");
-}
-
-var walkoutSlideIndex = 0;
-var panelSlideIndex = 0;
-var debateSlideIndex = 0;
-
-$(document).ready(function(){
-	walkoutSlideIndex = unhideSlide(walkoutSlideIndex, "walkoutSlides");
+$(document).ready(function() {
+	var slideshows = document.getElementsByClassName("slideshowContainer");
+	$(window).on("load", function() {
+		var instances = initSlides(slideshows);
+		for(var i=0; i<instances.length; i++) {
+			console.log("Image shown");
+			instances[i].imageArray[0].style.display = "block";
+		}
+	})
 })
